af94db07d4d3aca0865653934566b1fb6ee5e294
Juhui <21600763@handong.edu>
2021-05-01 16:01:25 +0900

    fixed #1

--- src/main/java/edu/handong/cafe/CafeEdiya.java
+++ src/main/java/edu/handong/cafe/CafeEdiya.java
@@ -1,12 +1,12 @@
 package edu.handong.cafe;
-
-public class CafeEdiya extends Cafe {
-
-	public CafeEdiya() {
-		System.out.println("\nWelcome to Ediya!!!");
-	}
-
-// 	public void givePoint() {
+//
+// public class CafeEdiya extends Cafe {
+//
+// 	public CafeEdiya() {
+// 		System.out.println("\nWelcome to Ediya!!!");
+// 	}
+//
+// // 	public void givePoint() {
 // 		System.out.println("Points for Ediya have been accumulated.");
 // 	}
 //
